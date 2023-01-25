fc8d683e03a6ecce9bfa254206f64ed8a2149b8f
Neon Karearea <46880415+NeonKarearea@users.noreply.github.com>
2023-01-25 20:02:48 +1300

    Delete puzzle-check-1.js

--- puzzle-check-1.js
+++
@@ -1,8 +0,0 @@
-function check() {
-	var ans = document.getElementById("answer").value;
-	if (ans === "battista") {
-		alert("Correct");
-	} else {
-		alert("Incorrect. Note: Answer is Case Sensitive");
-	}
-}
