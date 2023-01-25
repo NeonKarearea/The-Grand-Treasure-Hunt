fd04e78846f82ebc8b70614b3fb5d398d1ae9a86
Neon Karearea <46880415+NeonKarearea@users.noreply.github.com>
2023-01-25 15:06:00 +1300

    Update puzzle-check-2.js

--- puzzle-check-2.js
+++ puzzle-check-2.js
@@ -1,5 +1,5 @@
 function check() {
-	var ans = document.getElementById("answer").value {
+	var ans = document.getElementById("answer").value;
 	if (ans === "conquered") {
 		alert("Correct");
 	} else {
