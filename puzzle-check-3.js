5427236046e10225ee665a8a5213b3c145f2f60b
Neon Karearea <46880415+NeonKarearea@users.noreply.github.com>
2023-01-25 20:03:02 +1300

    Delete puzzle-check-3.js

--- puzzle-check-3.js
+++
@@ -1,8 +0,0 @@
-function check() {
-	var ans = document.getElementById("answer").value;
-	if (ans === "Alpha Cru") {
-		alert("Correct");
-	} else {
-		alert("Incorrect. Note: Answer is Case Sensitive")
-	}
-}
